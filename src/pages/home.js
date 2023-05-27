00402c8a7e510316ef53e4a124e5ae3b6cc94808
gabrielhenrique0 <gabrielcaldeira215@gmail.com>
2023-05-27 19:45:33 -0300

    (27/05) Iniciando front-end

---
+++ src/pages/home.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Keyboard, ActivityIndicator, ImageBackground, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+// import api from '../services/api';
+const image = { uri: 'https://cdn.discordapp.com/attachments/1093885046617812992/1112102502758879423/Background_MetaG2.png' };
+import { Container, Form, Input, SubmitButton, List, User, Avatar, Name, Bio, Description, ProfileButton, ProfileButtonText } from './styles';
+
+export default class Home extends Component {
+
+  render() {
+
+    return (
+      <Container>
+        <ImageBackground source={image} resizeMode="cover" style={{
+          width: '100%',
+          flex: 1,
+          justifyContent: 'center'
+        }}>
+        </ImageBackground>
+      </Container>
+    );
+  }
+}
