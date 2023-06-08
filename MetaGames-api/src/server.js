a23925c36420ca71d62b2a32fe2777c847c656e0
rafael <rafacilio00@gmail.com>
2023-06-08 17:07:42 -0300

    separação do front e do back

---
+++ MetaGames-api/src/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/health", (req, res) => {
+    return res.json("up");
+});
+
+app.listen(4000, () => console.log("server on localhost:4000"));
