e3b34b029383fc3dfcb0215e7f84962988b99be6
gabrielhenrique0 <gabrielcaldeira215@gmail.com>
2023-06-22 19:54:51 -0300

    (22/06) Buscando informações da API

--- MetaGames/src/pages/lista.js
+++ MetaGames/src/pages/lista.js
@@ -1,16 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity, FlatList } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
+import apiGames from "../service/apiGames";
 
 export default function Lista() {
-  const navigation = useNavigation()
-  const handleLista = () => (
-    navigation.navigate("Home")
-  )
+  const navigation = useNavigation();
+  const [gameList, setGameList] = useState([]);
+
+  const handleLista = () => {
+    navigation.navigate("Home");
+  };
+
+  useEffect(() => {
+    getGamesList();
+  }, []);
+
+  const getGamesList = () => {
+    apiGames.getAllGames.then((resp) => {
+      console.log(resp.data.results);
+      setGameList(resp.data.results);
+    });
+  };
+
+  const renderGameCard = ({ item }) => {
+    return (
+      <TouchableOpacity style={styles.card}>
+        <Text>{item.title}</Text>
+        <Text>{item.name}</Text>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <ImageBackground
       source={require('../../assets/FundoMetaGames.png')}
-      style={styles.background}>
+      style={styles.background}
+    >
       <View style={styles.container}>
         <View style={styles.logoContainer}>
           <TouchableOpacity onPress={handleLista}>
@@ -21,11 +47,15 @@ export default function Lista() {
             />
           </TouchableOpacity>
         </View>
-        <Text></Text>
+        <FlatList
+          data={gameList}
+          renderItem={renderGameCard}
+          keyExtractor={(item) => item.id.toString()}
+        />
         <StatusBar style="auto" />
       </View>
     </ImageBackground>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -47,5 +77,11 @@ const styles = StyleSheet.create({
   logo: {
     width: 100,
     height: 100,
+  },
+  card: {
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    padding: 16,
+    margin: 8,
   },
 });
